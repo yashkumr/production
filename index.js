34f47e9ec0b94cf904a90dcdeacc8388dcbc260b
vimlesh kumar <111264210+yashkumr@users.noreply.github.com>
2024-03-29 12:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ connectDB();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const __dirname = path.resolve();
+
 export const app = express();
 
 //middleware
